7b8a49ed0c555b2f192671a8b7fece7858c7fdd5
Gioele Iacovella <g.ioele96@outlook.it>
2023-12-13 15:43:35 +0100

    minor fix on pizza.java

--- src/main/java/org/java/spring/db/pojo/Pizza.java
+++ src/main/java/org/java/spring/db/pojo/Pizza.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -42,7 +43,7 @@ public class Pizza {
     @OneToMany(mappedBy = "pizza", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Discount> discounts;
     
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
 	private List<Ingredient> ingredients;
     
 	public Pizza() { }
