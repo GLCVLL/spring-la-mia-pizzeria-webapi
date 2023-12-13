f0e4c9790a00e553711ff142c1588ff0fd7659b2
Gioele Iacovella <g.ioele96@outlook.it>
2023-12-13 12:59:10 +0100

    showed single pizza

--- src/main/java/org/java/spring/restcontroller/PizzaRestController.java
+++ src/main/java/org/java/spring/restcontroller/PizzaRestController.java
@@ -42,39 +42,5 @@ public class PizzaRestController {
 		return new ResponseEntity<>(pizza, HttpStatus.OK);
 	}
 	
-	@PostMapping
-	public ResponseEntity<Pizza> create(@RequestBody Pizza pizza) {
-		
-		pizzaService.save(pizza);
-		
-		return new ResponseEntity<>(pizza, HttpStatus.OK);
-	}
-	
-	@PutMapping("{id}")
-	public ResponseEntity<Pizza> update(
-			@PathVariable int id,
-			@RequestBody Pizza newPizza) {
-	
-		Pizza pizza = pizzaService.findById(id);
-		
-		pizza.setName(newPizza.getName());
-		pizza.setDescription(newPizza.getDescription());
-		pizza.setPrice(newPizza.getPrice());
-		
-		pizzaService.save(pizza);
-		
-		return new ResponseEntity<>(pizza, HttpStatus.OK);
-	}
-	
-	@DeleteMapping("{id}")
-	public ResponseEntity<Pizza> delete(
-			@PathVariable int id) {
-		
-		Pizza pizza = pizzaService.findById(id);
-		
-		pizzaService.delete(pizza);
-		
-		return new ResponseEntity<>(pizza, HttpStatus.OK); 
-	}
 
 }
